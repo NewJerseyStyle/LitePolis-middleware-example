3bfc6175b558357aa727577a7176e10a7b47c3b7
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-04-02 15:11:03 -0400

    fix package name

--- litepolis_middleware_example/core.py
+++ litepolis_middleware_example/core.py
@@ -38,7 +38,11 @@ class JWTAuth(AuthenticationBackend):
             if ("PYTEST_CURRENT_TEST" not in os.environ and
                 "PYTEST_VERSION" not in os.environ):
                 # Replace with actual service name and key
-                package_name = os.path.basename(os.path.dirname(__file__))
+                package_name = os.path.basename(
+                    os.path.dirname(
+                        os.path.dirname(__file__)
+                    )
+                )
                 secret_key = get_config(package_name, "secret_key")
             else:
                 # Running under Pytest: Use default values
